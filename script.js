5f9a7ed0f465958265d036a4d8671235b9ecdbab
lennamiho <147773523+lennamiho@users.noreply.github.com>
2023-10-12 19:21:33 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nomeInput = document.getElementById("nome");
+    const nomesDiv = document.getElementById("nomes");
+
+    nomeInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter" && nomeInput.value.trim() !== "") {
+            const nome = nomeInput.value;
+            nomeInput.value = "";
+
+            const li = document.createElement("li");
+            li.textContent = nome;
+            nomesDiv.appendChild(li);
+            addEmoji(li);
+        }
+    });
+
+    function addEmoji(li) {
+        fetch('emojis.json')
+            .then(response => response.json())
+            .then(data => {
+                const emojisPredefinidos = data.emojis;
+                const emoji = emojisPredefinidos[Math.floor(Math.random() * emojisPredefinidos.length)];
+
+                const emojiSpan = document.createElement("span");
+                emojiSpan.textContent = emoji;
+                li.appendChild(emojiSpan);
+            })
+            .catch(error => {
+                console.error("Erro ao carregar emojis:", error);
+            });
+    }
+});
